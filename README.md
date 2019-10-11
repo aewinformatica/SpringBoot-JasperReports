907b3bc16c46e4489740d5b0da1a44380ca63d0f
WAGNER <aewinformatica@gmail.com>
2019-10-11 19:54:25 -0300

    Inclusão do Projeto Funcional

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # SpringBoot-JasperReports
-Simples Projeto com SpringBoot 2 e Relatório com JasperReports
+Simples Projeto com SpringBoot 2 e Relatório com JasperReports com opção de VISUALIZAR OU DOWNLOAD DO PDF
+
+## Get Start
+
+O projeto esta configurado para usar banco de dados Postgree SQL,
+crie uma base de dados chamada 
+**springjasper** e 
+execute a classe 
+**SpringJasperApplication.java**.
+
+
+Os dados de conexão podem ser alterados no *application.properties*: 
+
+spring.datasource.url=jdbc:postgresql://localhost:5432/springjasper
+spring.datasource.username=postgres
+
+http://localhost:8080
---
+++ src/main/java/com/github/aewinformatica/SpringJasper/controller/RelatorioController.java
@@ -0,0 +1,60 @@
+package com.github.aewinformatica.SpringJasper.controller;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.util.JRLoader;
+
+@Controller
+@RequestMapping("/relatorio")
+public class RelatorioController {
+	
+	@Autowired
+	private DataSource dataSource;
+
+	@PostMapping
+	public void imprimir(@RequestParam Map<String, Object> parametros, HttpServletResponse response) throws JRException, SQLException, IOException {
+		
+		parametros = parametros == null ? parametros = new HashMap<>() : parametros;
+		
+		
+		
+		// Pega o arquivo .jasper localizado em resources
+		InputStream jasperStream = this.getClass().getResourceAsStream("/relatorios/livros.jasper");
+		
+		// Cria o objeto JaperReport com o Stream do arquivo jasper
+		JasperReport jasperReport = (JasperReport) JRLoader.loadObject(jasperStream);
+		// Passa para o JasperPrint o relatório, os parâmetros e a fonte dos dados, no caso uma conexão ao banco de dados
+		JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parametros, dataSource.getConnection());
+
+		// Configura a respota para o tipo PDF
+		response.setContentType("application/pdf");
+		// Define que o arquivo pode ser visualizado no navegador e também nome final do arquivo
+		// para fazer download do relatório troque 'inline' por 'attachment'
+		String opcao = parametros.get("opcao").toString();
+		response.setHeader("Content-Disposition", opcao + "; filename=livros.pdf");
+
+		// Faz a exportação do relatório para o HttpServletResponse
+		final OutputStream outStream = response.getOutputStream();
+		JasperExportManager.exportReportToPdfStream(jasperPrint, outStream);
+	}
+
+}
